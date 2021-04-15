20d3ef7f7047940b4aeab7ae554fa97b1a58bbc4
Richard <rdesilvey@gmail.com>
2021-04-15 09:26:57 -0600

    Test that it has rides

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -4,12 +4,17 @@ require './lib/carnival'
 describe Carnival do
   describe '#new' do
     it 'creates a new instance' do
-      jeffco_fair = Carnival.new("Jefferson County Fair")
+      jeffco_fair = Carnival.new('Jefferson County Fair')
       expect(jeffco_fair).to be_instance_of Carnival
     end
     it 'has a name' do
-      jeffco_fair = Carnival.new("Jefferson County Fair")
+      jeffco_fair = Carnival.new('Jefferson County Fair')
       expect(jeffco_fair.name).to eq 'Jefferson County Fair'
+    end
+    it 'has rides' do
+      jeffco_fair = Carnival.new('Jefferson County Fair')
+      expect(jeffco_fair.rides).to be_instance_of Array
+      expect(jeffco_fair.rides).to eq []
     end
   end
 end
