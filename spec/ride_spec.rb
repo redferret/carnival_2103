e849da1b669715b96327201d40a280adccd4c4d5
Richard <rdesilvey@gmail.com>
2021-04-15 09:10:10 -0600

    Test that a ride has a name

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -7,5 +7,10 @@ describe Ride do
       ride = Ride.new({name: 'Ferris Wheel', cost: 0})
       expect(ride).to be_instance_of Ride
     end
+
+    it 'has a name' do
+      ride = Ride.new({name: 'Ferris Wheel', cost: 0})
+      expect(ride.name).to eq 'Ferris Wheel'
+    end
   end
 end
